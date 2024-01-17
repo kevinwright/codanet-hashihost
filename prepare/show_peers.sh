5ce2c941538bb376ae92c028a6a1fbfcac74d653
Kevin Wright <kev.lee.wright@gmail.com>
2024-01-17 17:59:28 +0000

    formatting

--- prepare/show_peers.sh
+++ prepare/show_peers.sh
@@ -5,6 +5,7 @@ avahi-browse -rtpl _hashicorp_consul_server._tcp | \
     awk '
       BEGIN { print  "┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓"
               print  "┃ Other hosts currently advertising are: ┃"
+              print  "┃                                        ┃"
       }
             { printf "┃ ● %-36s ┃\n", $8}
       END   { print  "┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛\n"}      
